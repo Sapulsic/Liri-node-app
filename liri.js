6ce0a39ff7a2f8002dba0fdbf7458ddbc62df5b7
Sapulsic <carlos.barahona1995@gmail.com>
2018-10-27 02:41:29 -0500

    Npm files, and keys, and JS

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var keysUnlock = require("./keys.js")
+let keysUnlock = require("./keys.js")
 
 var spotify = new Spotify(keys.spotify);
 
@@ -12,8 +12,10 @@ var search = process.argv[2];
 var term = process.argv.slice(3).join(" ");
 
 
-switch (key) {
+switch (search) {
     case "concert-this":
+
+        thisConcert(term);
 
         break;
     case "spotify-this-song":
@@ -28,4 +30,43 @@ switch (key) {
 
     default:
         break;
+}
+
+function thisConcert(term) {
+
+        request(queryURL, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+
+        }
+    });
+}
+
+function thisMovie(term) {
+    var queryURL = "http://www.omdbapi.com/?t=" + term + "&apikey=eaaa83c3";
+    request(queryURL, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+        console.log("Title of the movie: " + JSON.parse(body).Title);
+        console.log("Year the movie came out: " + JSON.parse(body).Year);
+        console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).ratings[1].Value);
+        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+        console.log("Language of the movie: " + JSON.parse(body).Language);
+        console.log("Plot of the movie: " + JSON.parse(body).Plot);
+        console.log("Actors in the movie: " + JSON.parse(body).Actors);
+    }
+}); 
+}
+
+function spotifySong(term) {
+
+    request(queryURL, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+    }
+});
 }
