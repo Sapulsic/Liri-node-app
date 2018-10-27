90630b5be10b20a5465a8013030e5b57de5a3e08
Sapulsic <carlos.barahona1995@gmail.com>
2018-10-27 01:47:44 -0500

    directory changes

---
+++ liri.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+var keysUnlock = require("./keys.js")
+
+var spotify = new Spotify(keys.spotify);
+
+var momentJS = require("moment");
+var request = require("request");
+
+
+var search = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+
+
+switch (key) {
+    case "concert-this":
+
+        break;
+    case "spotify-this-song":
+
+        break;
+    case "movie-this":
+
+        break;
+    case "do-what-it-says" :
+
+    break;
+
+    default:
+        break;
+}
