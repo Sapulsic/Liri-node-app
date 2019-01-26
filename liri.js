db96e79abb763454f8f39bcabb6696e680dd12c2
Sapulsic <carlos.barahona1995@gmail.com>
2019-01-26 00:33:53 -0600

    made changes

--- liri.js
+++ liri.js
@@ -69,7 +69,7 @@ function thisMovie(term) {
             console.log("Title of the movie: " + JSON.parse(body).Title);
             console.log("Year the movie came out: " + JSON.parse(body).Year);
             console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).ratings[1].Value);
+            console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
             console.log("Country where the movie was produced: " + JSON.parse(body).Country);
             console.log("Language of the movie: " + JSON.parse(body).Language);
             console.log("Plot of the movie: " + JSON.parse(body).Plot);
